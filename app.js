481083f0b7e0ab9d0da3ab3c3e054ce996fda3a2
Chinonso Peace Mbama <mbamachinonso@gmail.com>
2022-10-10 23:27:29 +0100

    more changes

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+// ACTIVITY 1
+const toggleBtn = document.getElementById('submit');
+const toggleImg = document.getElementById('landscapeImg');
+
+toggleBtn.addEventListener('click', () => {
+    if(toggleImg.style.display === "none"){
+        toggleImg.style.display = "block";
+    } else {
+        toggleImg.style.display = "none";
+    }
+})
+
+
+// ACTIVITY 3
+
 const heading = document.getElementById('heading')
 const input = document.getElementById('input')
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    heading.style.color = input.value
+})
